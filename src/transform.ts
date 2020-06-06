fcaacc42f886c428e532068c25b086e0466a913b
pooya parsa <pooya@pi0.ir>
2020-06-07 01:42:06 +0200

    fix: directly add typrscript transform

--- src/transform.ts
+++ src/transform.ts
@@ -1,15 +1,16 @@
 import { transformSync } from '@babel/core'
 // @ts-ignore
-import babelPresetEnv from '@babel/preset-env'
+import env from '@babel/preset-env'
+// @ts-ignore
+import typescript from '@babel/plugin-transform-typescript'
 
 export function transform (source: string): string {
   const result = transformSync(source, {
     presets: [
-      [babelPresetEnv, {
-        targets: {
-          node: 'current'
-        }
-      }]
+      [env, { targets: { node: 'current' } }]
+    ],
+    plugins: [
+      typescript
     ]
   })?.code || ''
 
