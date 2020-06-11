4b643be5390c9617e05faf576629244201247f31
pooya parsa <pooya@pi0.ir>
2020-06-11 16:12:10 +0200

    chore: update test for stack

--- test/fixtures/esm/test.js
+++ test/fixtures/esm/test.js
@@ -1,6 +1,9 @@
+const getStack = () => new Error('Boo').stack
+
 export default function test () {
   return {
     file: __filename,
-    dir: __dirname
+    dir: __dirname,
+    stack: getStack().split('\n').splice(1).map(s => s.trim())
   }
 }
