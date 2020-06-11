ae4dc58ab994419489c4599c04c3444a34ba6215
pooya parsa <pooya@pi0.ir>
2020-06-11 16:11:45 +0200

    feat: improve babel options
    - don't inherit project babelrc
    - always disable compact
    - use retainLines to keep correct line number in stack traces

--- src/babel.ts
+++ src/babel.ts
@@ -4,13 +4,17 @@ import commonjs from '@babel/plugin-transform-modules-commonjs'
 // @ts-ignore
 import typescript from '@babel/plugin-transform-typescript'
 
-export function transform (source: string): string {
+export function transform (source: string, filename?: string): string {
   const result = transformSync(source, {
+    babelrc: false,
+    compact: false,
+    retainLines: true,
+    filename,
     plugins: [
       [commonjs, { allowTopLevelThis: true }],
       typescript
     ]
   })?.code || ''
-
+  // console.log(result)
   return result
 }
