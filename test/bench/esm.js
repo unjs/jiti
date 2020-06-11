a74b0cc3c35e42a4903da46298daa60eead17724
pooya parsa <pooya@pi0.ir>
2020-06-11 22:54:21 +0200

    esm: enable cache

--- test/bench/esm.js
+++ test/bench/esm.js
@@ -1,8 +1,8 @@
 console.time('esm_init')
-const esm = require('esm')(module, { cache: false })
+const esm = require('esm')(module)
 console.timeEnd('esm_init')
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 2; i++) {
   console.time('esm_require')
   esm('../fixtures/esm').test()
   console.timeEnd('esm_require')
