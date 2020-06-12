c4bff825667b96d6bd76ed76c0324441e8f4844d
Daniel Roe <daniel@concision.co.uk>
2020-06-12 15:37:53 +0100

    test: disable caching for benchmarks

--- test/bench/suite.js
+++ test/bench/suite.js
@@ -1,23 +1,30 @@
-const Benchmark = require('benchmark')
+const Benchmark = requireUncached('benchmark')
 const suite = new Benchmark.Suite()
 
+process.setMaxListeners(10000)
+
+function requireUncached (module) {
+  delete require.cache[require.resolve(module)]
+  return require(module)
+}
+
 const transformers = [
-  ['babel', () => undefined],
-  ['esbuild-async', () => require('../../dist/esbuild-async')],
-  ['esbuild-sync', () => require('../../dist/esbuild-sync')]
+  ['babel', () => requireUncached('../../dist/babel')],
+  ['esbuild-async', () => requireUncached('../../dist/esbuild-async')],
+  ['esbuild-sync', () => requireUncached('../../dist/esbuild-sync')]
 ]
 
 const test = process.argv[2]
 
 if (test === 'fixture') {
   suite.add('esm js', () => {
-    const esm = require('esm')(module)
+    const esm = requireUncached('esm')(module)
     for (let i = 0; i < 3; i++) {
       esm('../fixtures/esm').test()
     }
   })
 } else {
-  suite.add('esm init', () => require('esm')(module))
+  suite.add('esm init', () => requireUncached('esm')(module))
 }
 
 transformers
@@ -25,14 +32,14 @@ transformers
   .forEach(([name, fn]) => {
     if (test === 'fixture') {
       suite.add(`${name} js`, () => {
-        const jiti = require('../..')(__filename, { transform: fn() })
+        const jiti = requireUncached('../..')(__filename, { transform: fn(), cache: false })
 
         for (let i = 0; i < 3; i++) {
           jiti('../fixtures/esm').test()
         }
       })
       suite.add(`${name} ts`, () => {
-        const jiti = require('../..')(__filename, { transform: fn() })
+        const jiti = requireUncached('../..')(__filename, { transform: fn(), cache: false })
 
         for (let i = 0; i < 3; i++) {
           jiti('../fixtures/typescript').test()
@@ -40,7 +47,7 @@ transformers
       })
     } else {
       suite.add(`${name} init`, () =>
-        require('../..')(__filename, { transform: fn() })
+        requireUncached('../..')(__filename, { transform: fn(), cache: false })
       )
     }
   })
