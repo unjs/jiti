4ea863b217c9bda453f0e8cbb09b04dea16a4f59
pooya parsa <pooya@pi0.ir>
2020-06-12 14:20:12 +0200

    refactor: remove duplicate defaults and clone opts

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-module.exports = function (filename = process.cwd(), opts = {}) {
+module.exports = function (filename, opts) {
   require('./dist/v8cache')
   const jiti = require('./dist/jiti')
+
+  opts = { ...opts }
 
   if (!opts.transform) {
     opts.transform = require('./dist/babel')
