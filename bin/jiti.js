52a4c124bc649b366d481cbe6463537b245ad4d1
Pooya Parsa <pyapar@gmail.com>
2020-11-27 23:42:26 +0100

    fix(cli): resolve relative to cwd

--- bin/jiti.js
+++ bin/jiti.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const { resolve } = require('path')
+
 const script = process.argv.splice(2, 1)[0]
 
 if (!script) {
@@ -8,6 +10,7 @@ if (!script) {
   process.exit(1)
 }
 
-const jiti = require('..')(process.cwd())
-const resolved = process.argv[1] = jiti.resolve(script)
+const pwd = process.cwd()
+const jiti = require('..')(pwd)
+const resolved = process.argv[1] = jiti.resolve(resolve(pwd, script))
 jiti(resolved)
