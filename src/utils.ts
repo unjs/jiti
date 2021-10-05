4c0deca11a0e68d7b1584d69451402230766f36a
Daniel Roe <daniel@roe.dev>
2021-10-05 23:52:27 +0200

    refactor: extract `interopDefault` into `mlly` (#45)

--- src/utils.ts
+++ src/utils.ts
@@ -20,33 +20,6 @@ export function isWritable (filename: string): boolean {
   }
 }
 
-export function interopDefault (sourceModule: any): any {
-  if (!(sourceModule && 'default' in sourceModule)) {
-    return sourceModule
-  }
-  const newModule = sourceModule.default
-  for (const key in sourceModule) {
-    if (key === 'default') {
-      try {
-        Object.defineProperty(newModule, key, {
-          enumerable: false,
-          configurable: false,
-          get () { return newModule }
-        })
-      } catch (_err) {}
-    } else {
-      try {
-        Object.defineProperty(newModule, key, {
-          enumerable: true,
-          configurable: true,
-          get () { return sourceModule[key] }
-        })
-      } catch (_err) {}
-    }
-  }
-  return newModule
-}
-
 export function md5 (content: string, len = 8) {
   return createHash('md5').update(content).digest('hex').substr(0, len)
 }
