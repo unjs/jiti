1faddf4bec8219d3d09d4ff903a6183c77be342f
Anthony Fu <anthonyfu117@hotmail.com>
2022-01-25 16:42:12 +0800

    chore: make snapshot stable

--- test/fixtures.test.ts
+++ test/fixtures.test.ts
@@ -1,4 +1,4 @@
-import { join, resolve, dirname } from 'path'
+import { join, resolve, dirname } from 'pathe'
 import { execa } from 'execa'
 import { describe, it, expect } from 'vitest'
 import fg from 'fast-glob'
@@ -14,9 +14,19 @@ describe('fixtures', async () => {
     it(dirname(fixture), async () => {
       const fixtureEntry = join(dir, fixture)
       const cwd = dirname(fixtureEntry)
+
+      // Clean up absolute paths and sourcemap locations for stable snapshots
+      function cleanUpSnap (str:string) {
+        return str
+          .replaceAll(/\\/g, '/')
+          .replaceAll(cwd, '<cwd>')
+          .replaceAll(root, '<root>')
+          .replace(/:\d+:\d+([)'\n])/g, '$1')
+      }
+
       const { stdout, stderr } = await execa('node', [jitiPath, fixtureEntry], { cwd, stdio: 'pipe', reject: false })
-      expect(stdout.replaceAll(cwd, '<cwd>').replaceAll(root, '<root>')).toMatchSnapshot('stdout')
-      expect(stderr.replaceAll(cwd, '<cwd>').replaceAll(root, '<root>')).toMatchSnapshot('stderr')
+      expect(cleanUpSnap(stdout)).toMatchSnapshot('stdout')
+      expect(cleanUpSnap(stderr)).toMatchSnapshot('stderr')
     })
   }
 })
