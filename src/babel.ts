395872852d1f754f03ee52b15b020ec2e3223835
Daniel Roe <daniel@roe.dev>
2022-06-17 15:50:36 +0100

    fix: use inlined import meta plugin to inject url

--- src/babel.ts
+++ src/babel.ts
@@ -1,3 +1,7 @@
+import { pathToFileURL } from 'url'
+import { smart } from '@babel/template'
+import type { NodePath, PluginObj } from '@babel/core'
+import type { Statement, MemberExpression } from '@babel/types'
 import { transformSync, TransformOptions as BabelTransformOptions } from '@babel/core'
 import { TransformOptions, TRANSFORM_RESULT } from './types'
 
@@ -13,7 +17,7 @@ export default function transform (opts: TransformOptions): TRANSFORM_RESULT {
     plugins: [
       [require('@babel/plugin-transform-modules-commonjs'), { allowTopLevelThis: true }],
       [require('babel-plugin-dynamic-import-node'), { noInterop: true }],
-      [require('babel-plugin-transform-import-meta')],
+      [TransformImportMetaPlugin, { filename: opts.filename }],
       [require('@babel/plugin-syntax-class-properties')]
     ]
   }
@@ -48,6 +52,46 @@ export default function transform (opts: TransformOptions): TRANSFORM_RESULT {
         code: err.code?.replace('BABEL_', '').replace('PARSE_ERROR', 'ParseError'),
         message: err.message?.replace('/: ', '').replace(/\(.+\)\s*$/, '')
       })
+    }
+  }
+}
+
+// https://github.com/javiertury/babel-plugin-transform-import-meta/blob/master/src/index.ts
+const TransformImportMetaPlugin = (opts: { filename?: string }): PluginObj => {
+  return {
+    name: 'transform-import-meta',
+    visitor: {
+      Program (path) {
+        const metas: Array<NodePath<MemberExpression>> = []
+
+        path.traverse({
+          MemberExpression (memberExpPath) {
+            const { node } = memberExpPath
+
+            if (
+              node.object.type === 'MetaProperty' &&
+              node.object.meta.name === 'import' &&
+              node.object.property.name === 'meta' &&
+              node.property.type === 'Identifier' &&
+              node.property.name === 'url'
+            ) {
+              metas.push(memberExpPath)
+            }
+          }
+        })
+
+        if (metas.length === 0) {
+          return
+        }
+
+        for (const meta of metas) {
+          meta.replaceWith(smart.ast`${
+            opts.filename
+              ? JSON.stringify(pathToFileURL(opts.filename))
+              : "require('url').pathToFileURL(__filename).toString()"
+          }` as Statement)
+        }
+      }
     }
   }
 }
