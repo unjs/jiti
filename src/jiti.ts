e7f40729d56f1671b7fae7c4b924b5b971bf9ca9
webstrand <webstrand@gmail.com>
2023-01-30 20:08:58 -0500

    feat: add support for `.cts`
    Co-authored-by: pooya parsa <pyapar@gmail.com>

--- src/jiti.ts
+++ src/jiti.ts
@@ -43,7 +43,7 @@ const defaults: JITIOptions = {
   esmResolve: _EnvESMResolve || false,
   cacheVersion: "7",
   legacy: lt(process.version || "0.0.0", "14.0.0"),
-  extensions: [".js", ".mjs", ".cjs", ".ts", ".mts"],
+  extensions: [".js", ".mjs", ".cjs", ".ts", ".mts", ".cts"],
   alias: _EnvAlias,
   nativeModules: _EnvNative || [],
   transformModules: _EnvTransform || [],
@@ -286,7 +286,7 @@ export default function createJITI(
     let source = readFileSync(filename, "utf8");
 
     // Transpile
-    const isTypescript = ext === ".ts" || ext === ".mts";
+    const isTypescript = ext === ".ts" || ext === ".mts" || ext === ".cts";
     const isNativeModule =
       ext === ".mjs" ||
       (ext === ".js" && readNearestPackageJSON(filename)?.type === "module");
