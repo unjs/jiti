09fdd32da73cf8baa776f89997a01bf61bc002aa
Pooya Parsa <pooya@pi0.io>
2023-02-08 21:33:01 +0100

    feat: support `.json` imports and import assertions

---
+++ test/fixtures/json/index.ts
@@ -0,0 +1,13 @@
+import imported from './file.json';
+import importedWithAssertion from './file.json' assert { type: 'json' };
+
+const required = require('./file.json');
+
+const debug = (label: string, value) => console.log(label, ':', value, '.default:', value.default);
+
+debug('Imported', imported);
+debug('Imported with assertion', importedWithAssertion);
+
+debug('Required', required);
+import('./file.json').then(r => debug('Dynamic Imported', r));
+
