e947becd5b815bfc27d14073a607f2dc72487a6e
Peter <peter.placzek1996@gmail.com>
2023-02-20 12:15:14 +0100

    fix: add support to emit decorator metadata (#119)

--- test/fixtures/typescript/index.ts
+++ test/fixtures/typescript/index.ts
@@ -1,4 +1,5 @@
 import test, { FeedService } from "./test";
+import Clazz from "./decorators";
 export type { Test } from "./types";
 
-console.log(test(), FeedService);
+console.log(test(), FeedService, Clazz);
