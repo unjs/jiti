33f8316538e4e2dea93c6eefd916892b4c39ea2f
Pooya Parsa <pooya@pi0.io>
2023-03-06 20:15:11 +0100

    test skip cache dir test on windows
    we might enabled back by either mocking tmpDir for windows or having a variant

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,9 +1,9 @@
 import { afterEach, describe, beforeEach, it, expect, vi } from "vitest";
-
+import { isWindows } from 'std-env'
 import { getCacheDir } from "../src/utils";
 
 describe("utils", () => {
-  describe("getCacheDir", () => {
+  describe.skipIf(isWindows)("getCacheDir", () => {
     const cwd = "/cwd";
     const notCwd = `${cwd}__NOT__`;
 
