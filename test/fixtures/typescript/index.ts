838bf9adbff0c5efb34912975ebfc26240865e14
Sabin Marcu <sabinmarcu@gmail.com>
2023-03-06 20:55:01 +0200

    test: add typescript satisfies fixture (#107)

--- test/fixtures/typescript/index.ts
+++ test/fixtures/typescript/index.ts
@@ -1,5 +1,8 @@
 import test, { FeedService } from "./test";
 import Clazz from "./decorators";
+import { test as satisfiesTest } from "./satisfies";
+
 export type { Test } from "./types";
 
 console.log(test(), FeedService, Clazz);
+console.log(satisfiesTest());
