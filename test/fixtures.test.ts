912c9d5c06c74945175430e1704e921c7fe64725
Pooya Parsa <pooya@pi0.io>
2023-05-24 15:58:06 +0200

    update test (timeout, lint)

--- test/fixtures.test.ts
+++ test/fixtures.test.ts
@@ -12,46 +12,54 @@ describe("fixtures", async () => {
 
   for (const fixture of fixtures) {
     const name = dirname(fixture);
-    it(name, async () => {
-      const fixtureEntry = join(dir, fixture);
-      const cwd = dirname(fixtureEntry);
+    it(
+      name,
+      async () => {
+        const fixtureEntry = join(dir, fixture);
+        const cwd = dirname(fixtureEntry);
 
-      // Clean up absolute paths and sourcemap locations for stable snapshots
-      function cleanUpSnap(str: string) {
-        return (str + "\n")
-          .replace(/\n\t/g, "\n")
-          .replace(/\\+/g, "/")
-          .split(cwd.replace(/\\/g, "/"))
-          .join("<cwd>") // workaround for replaceAll in Node 14
-          .split(root.replace(/\\/g, "/"))
-          .join("<root>") // workaround for replaceAll in Node 14
-          .replace(/:\d+:\d+([\s')])/g, "$1") // remove line numbers in stacktrace
-          .replace(/node:(internal|events)/g, "$1") // in Node 16 internal will be presented as node:internal
-          .replace(/\.js\)/g, ")")
-          .replace(/file:\/{3}/g, "file://")
-          .replace(/ParseError: \w:\/:\s+/, "ParseError: ") // Unknown chars in Windows
-          .trim();
-      }
+        // Clean up absolute paths and sourcemap locations for stable snapshots
+        function cleanUpSnap(str: string) {
+          return (str + "\n")
+            .replace(/\n\t/g, "\n")
+            .replace(/\\+/g, "/")
+            .split(cwd.replace(/\\/g, "/"))
+            .join("<cwd>") // workaround for replaceAll in Node 14
+            .split(root.replace(/\\/g, "/"))
+            .join("<root>") // workaround for replaceAll in Node 14
+            .replace(/:\d+:\d+([\s')])/g, "$1") // remove line numbers in stacktrace
+            .replace(/node:(internal|events)/g, "$1") // in Node 16 internal will be presented as node:internal
+            .replace(/\.js\)/g, ")")
+            .replace(/file:\/{3}/g, "file://")
+            .replace(/ParseError: \w:\/:\s+/, "ParseError: ") // Unknown chars in Windows
+            .trim();
+        }
 
-      const { stdout, stderr } = await execa("node", [jitiPath, fixtureEntry], {
-        cwd,
-        stdio: "pipe",
-        reject: false,
-        env: {
-          JITI_CACHE: "false",
-          JITI_REQUIRE_CACHE: "false",
-          JITI_ESM_RESOLVE: "true",
-        },
-      });
+        const { stdout, stderr } = await execa(
+          "node",
+          [jitiPath, fixtureEntry],
+          {
+            cwd,
+            stdio: "pipe",
+            reject: false,
+            env: {
+              JITI_CACHE: "false",
+              JITI_REQUIRE_CACHE: "false",
+              JITI_ESM_RESOLVE: "true",
+            },
+          }
+        );
 
-      if (name.includes("error")) {
-        expect(cleanUpSnap(stderr)).toMatchSnapshot("stderr");
-      } else {
-        // expect no error
-        expect(stderr).toBe("");
-      }
+        if (name.includes("error")) {
+          expect(cleanUpSnap(stderr)).toMatchSnapshot("stderr");
+        } else {
+          // expect no error
+          expect(stderr).toBe("");
+        }
 
-      expect(cleanUpSnap(stdout)).toMatchSnapshot("stdout");
-    });
+        expect(cleanUpSnap(stdout)).toMatchSnapshot("stdout");
+      },
+      60_000
+    );
   }
 });
