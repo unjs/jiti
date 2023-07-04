c6798c605bb860d8602d584ca5019e773c04c1ae
Pooya Parsa <pooya@pi0.io>
2023-07-04 13:12:27 +0200

    chore: fix vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    coverage: {
-      reporter: ["text", "clover", "json"],
-    },
+    exclude: ["**/test.{ts,mjs,cjs,js}", "node_modules/**/*"],
   },
 });
