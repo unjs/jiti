d5049ef546eec2ef957fefc8e867221d357f3dcd
Pooya Parsa <pooya@pi0.io>
2023-08-09 13:54:21 +0200

    fix tests

--- test/fixtures/env/index.ts
+++ test/fixtures/env/index.ts
@@ -1,4 +1,3 @@
 console.log("process.env.TEST", process.env.TEST);
-
 // @ts-ignore
-console.log("import.meta.env.TEST", import.meta.env?.TEST);
+console.log("import.meta.env.TEST", import.meta.env.TEST);
