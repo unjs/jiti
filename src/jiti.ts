5c3c69473d8543bac63a20e06b00b840bc08a34b
Anthony Fu <anthonyfu117@hotmail.com>
2023-10-02 05:03:45 -0500

    feat: add `jiti.import` function for async import (#170)

--- src/jiti.ts
+++ src/jiti.ts
@@ -69,6 +69,8 @@ export interface JITI extends Require {
   transform: (opts: TransformOptions) => string;
   register: () => () => void;
   evalModule: (source: string, options?: EvalModuleOptions) => unknown;
+  /** @experimental Behavior of `jiti.import` might change in the future. */
+  import: (id: string) => Promise<unknown>;
 }
 
 const JS_EXT_RE = /\.(c|m)?j(sx?)$/;
@@ -487,6 +489,7 @@ export default function createJITI(
   jiti.transform = transform;
   jiti.register = register;
   jiti.evalModule = evalModule;
+  jiti.import = async (id: string) => await jiti(id);
 
   return jiti;
 }
