9088580928b51d8b6bc958c77eac5a701d1ba7c7
Pooya Parsa <pyapar@gmail.com>
2023-10-02 12:52:33 +0200

    refactor: split option normalization (#172)

---
+++ src/options.ts
@@ -0,0 +1,45 @@
+import { destr } from "destr";
+import { lt } from "semver";
+import objectHash from "object-hash";
+
+import type { JITIOptions } from "./types";
+
+const _EnvDebug = destr<boolean>(process.env.JITI_DEBUG);
+const _EnvCache = destr<boolean>(process.env.JITI_CACHE);
+const _EnvESMResolve = destr<boolean>(process.env.JITI_ESM_RESOLVE);
+const _EnvRequireCache = destr<boolean>(process.env.JITI_REQUIRE_CACHE);
+const _EnvSourceMaps = destr<boolean>(process.env.JITI_SOURCE_MAPS);
+const _EnvAlias = destr<Record<string, string>>(process.env.JITI_ALIAS);
+const _EnvTransform = destr<string[]>(process.env.JITI_TRANSFORM_MODULES);
+const _EnvNative = destr<string[]>(process.env.JITI_NATIVE_MODULES);
+const _ExpBun = destr<string[]>(process.env.JITI_EXPERIMENTAL_BUN);
+
+const jitiDefaults: JITIOptions = {
+  debug: _EnvDebug,
+  cache: _EnvCache === undefined ? true : !!_EnvCache,
+  requireCache: _EnvRequireCache === undefined ? true : !!_EnvRequireCache,
+  sourceMaps: _EnvSourceMaps === undefined ? false : !!_EnvSourceMaps,
+  interopDefault: false,
+  esmResolve: _EnvESMResolve || false,
+  cacheVersion: "7",
+  legacy: lt(process.version || "0.0.0", "14.0.0"),
+  extensions: [".js", ".mjs", ".cjs", ".ts", ".mts", ".cts", ".json"],
+  alias: _EnvAlias,
+  nativeModules: _EnvNative || [],
+  transformModules: _EnvTransform || [],
+  experimentalBun: _ExpBun === undefined ? !!process.versions.bun : !!_ExpBun,
+};
+
+export function resolveJitiOptions(userOptions: JITIOptions): JITIOptions {
+  const opts: JITIOptions = { ...jitiDefaults, ...userOptions };
+
+  // Cache dependencies
+  if (opts.legacy) {
+    opts.cacheVersion += "-legacy";
+  }
+  if (opts.transformOptions) {
+    opts.cacheVersion += "-" + objectHash(opts.transformOptions);
+  }
+
+  return opts;
+}
