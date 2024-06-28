9a36d0ec67b6aa1ddc000e67e97962ddff2ce1af
Pooya Parsa <pooya@pi0.io>
2024-06-28 11:44:42 +0200

    chore: fix type issue

--- src/require.ts
+++ src/require.ts
@@ -28,7 +28,7 @@ export function jitiRequire(ctx: Context, id: string, async: boolean) {
     try {
       debug(ctx, `[bun] [native] ${id}`);
       id = jitiResolve(ctx, id);
-      if (async) {
+      if (async && ctx.nativeImport) {
         return ctx.nativeImport(id).then((m: any) => {
           if (ctx.opts.requireCache === false) {
             delete ctx.nativeRequire.cache[id];
