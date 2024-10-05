5c7795a72abd6b385fe52535fa66d98fa640ef5f
Pooya Parsa <pooya@pi0.io>
2024-10-05 11:37:32 +0200

    fix(interopDefault): avoid unnecessary `in` operator

--- src/utils.ts
+++ src/utils.ts
@@ -103,7 +103,7 @@ export function jitiInteropDefault(ctx: Context, mod: any) {
 }
 
 function interopDefault(mod: any): any {
-  if (!mod || !("default" in mod)) {
+  if (!mod) {
     return mod;
   }
 
