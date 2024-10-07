8d8081abce8f787f80e9322d5a6ed5a7635e8034
Lars Kappert <lars@webpro.nl>
2024-10-07 19:04:20 +0200

    Remove some unused exports

--- src/utils.ts
+++ src/utils.ts
@@ -33,10 +33,6 @@ export function md5(content: string, len = 8) {
   return createHash("md5").update(content).digest("hex").slice(0, len);
 }
 
-export function isObject(val: any) {
-  return val !== null && typeof val === "object";
-}
-
 export function readNearestPackageJSON(path: string): PackageJson | undefined {
   while (path && path !== "." && path !== "/") {
     path = join(path, "..");
