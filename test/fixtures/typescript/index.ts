8b8b74263a9b8c23aa5dc88583c8c0e20c46c1ba
Pooya Parsa <pooya@pi0.io>
2025-07-24 11:58:32 +0200

    fix ts issue

--- test/fixtures/typescript/index.ts
+++ test/fixtures/typescript/index.ts
@@ -2,6 +2,7 @@ import test, { type FeedService as _FeedService } from "./test";
 import Clazz from "./decorators";
 import { test as satisfiesTest } from "./satisfies";
 import { child } from "./parent.mjs";
+// @ts-expect-error (needs allowImportingTsExtensions)
 import defPromise from "./def-promise.cts";
 
 export type { Test } from "./types";
