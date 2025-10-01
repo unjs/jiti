054e532fd9a018e55e5d88580b1563013d5a2dc5
Kirk Waiblinger <53019676+kirkwaiblinger@users.noreply.github.com>
2025-10-01 11:31:58 -0600

    tweaks

--- test/fixtures/filename/index.ts
+++ test/fixtures/filename/index.ts
@@ -1,4 +1,3 @@
-// Test to demonstrate filename differences between CJS and ESM modules
 import { createRequire } from "node:module";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
@@ -7,11 +6,11 @@ const require = createRequire(import.meta.url);
 
 {
   // Test CJS module
-  const cjsResult = require("./cjs-module.cjs");
-  const cjsStackPath = cjsResult.stackTop.getFileName();
-  console.log("CJS __filename:", cjsResult.filename);
+  const cjs = require("./cjs-module.cjs");
+  const cjsStackPath = cjs.stackTop.getFileName();
+  console.log("CJS __filename:", cjs.filename);
   console.log("CJS filename in stack:", cjsStackPath);
-  console.log("These are equal?", cjsResult.filename === cjsStackPath);
+  console.log("These are equal?", cjs.filename === cjsStackPath);
   console.log(
     "CJS filename in stack is resolved on current platform?",
     path.resolve(cjsStackPath) === cjsStackPath,
@@ -20,11 +19,11 @@ const require = createRequire(import.meta.url);
 
 {
   // Test CTS module
-  const ctsResult = require("./cts-module.cts");
-  const ctsStackPath = ctsResult.stackTop.getFileName();
-  console.log("CTS __filename:", ctsResult.filename);
+  const cts = require("./cts-module.cts");
+  const ctsStackPath = cts.stackTop.getFileName();
+  console.log("CTS __filename:", cts.filename);
   console.log("CTS filename in stack:", ctsStackPath);
-  console.log("These are equal?", ctsResult.filename === ctsStackPath);
+  console.log("These are equal?", cts.filename === ctsStackPath);
   console.log(
     "CTS filename in stack is resolved on current platform?",
     path.resolve(ctsStackPath) === ctsStackPath,
@@ -33,11 +32,11 @@ const require = createRequire(import.meta.url);
 
 {
   // Test ESM module
-  const esmResult = await import("./esm-module.mjs");
-  const esmStackUrl = esmResult.stackTop.getFileName();
-  console.log("ESM URL:", esmResult.url);
+  const esm = await import("./esm-module.mjs");
+  const esmStackUrl = esm.stackTop.getFileName();
+  console.log("ESM URL:", esm.url);
   console.log("ESM filename in stack:", esmStackUrl);
-  console.log("These are equal?", esmResult.url === esmStackUrl);
+  console.log("These are equal?", esm.url === esmStackUrl);
   const esmStackUrlAsPath = fileURLToPath(esmStackUrl);
   console.log(
     "ESM URL as path in stack is resolved on current platform?",
@@ -48,11 +47,11 @@ const require = createRequire(import.meta.url);
 {
   // Test MTS module
   // @ts-expect-error -- allow importing TS extension should probably be enabled?
-  const mtsResult = await import("./mts-module.mts");
-  const mtsStackUrl = mtsResult.stackTop.getFileName();
-  console.log("MTS URL:", mtsResult.url);
+  const mts = await import("./mts-module.mts");
+  const mtsStackUrl = mts.stackTop.getFileName();
+  console.log("MTS URL:", mts.url);
   console.log("MTS URL in stack:", mtsStackUrl);
-  console.log("These are equal?", mtsResult.url === mtsStackUrl);
+  console.log("These are equal?", mts.url === mtsStackUrl);
   const mtsStackUrlAsPath = fileURLToPath(mtsStackUrl);
   console.log(
     "MTS filename in stack is resolved on current platform?",
