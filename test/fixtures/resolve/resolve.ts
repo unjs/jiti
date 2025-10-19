2a3702aee75b8e0b55091b4c5d292b92278add16
kricsleo <kricsleo@163.com>
2025-10-19 19:20:03 +0800

    test: update tests

--- test/fixtures/resolve/resolve.ts
+++ test/fixtures/resolve/resolve.ts
@@ -1,6 +1,6 @@
 const path = require("node:path");
 
 const resolvedPath = require.resolve("./foo.mjs", {
-  paths: [path.resolve(__dirname, "./dist")],
+  paths: [path.resolve(__dirname, "./_dist")],
 });
 console.log("resolved path", resolvedPath);
