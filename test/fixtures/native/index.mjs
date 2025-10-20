7f2934b965e8b0125702c56e7c9877263626c1ce
kricsleo <kricsleo@163.com>
2025-10-20 11:58:21 +0800

    test: fix tests

--- test/fixtures/native/index.mjs
+++ test/fixtures/native/index.mjs
@@ -1,7 +1,7 @@
 import { createJiti } from "../../../lib/jiti.cjs";
 
 async function main() {
-  import("./test.mjs").then(console.log);
+  await import("./test.mjs").then(console.log);
 
   const jiti = createJiti(import.meta.url, { tryNative: true });
   await jiti.import("./foo.ts");
